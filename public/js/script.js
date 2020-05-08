f20e41d8a0ba9353040a84cb577774a2de9eaa3a
CristhianChaucaMariaca <cristhiancchm+git@gmail.com>
2020-05-08 17:33:24 -0400

    se añadio el loader, se modifico el logo para una mejor presentación, se modifico la fuente de flaticon para poder manipular los tamaños en los iconos, se modifico footer y el social link, se modifico el tipo de letra para los titulos en un formato Script menos complicado de google-fonts

--- public/js/script.js
+++ public/js/script.js
@@ -1,48 +1,19 @@
-// $(document).ready(function () {
-//     $('#map-sanfrancisco-btn').click(function () { 
-//         $('#sucursal-sanfrancisco').fadeIn();
-//         $('#close-btn').click(function(){
-//             $('#sucursal-sanfrancisco').fadeOut();
-//         });
-//     });
-//     $('#map-sanfrancisco-btn').hover(function () {
-//             $('#sanfrancisco-icon').css("transform","rotate(10deg)");   
-//         }, function () {
-//             $('#sanfrancisco-icon').css("transform","rotate(0deg)");   
-//         }
-//     );
-//     $('#map-sopocachi-btn').hover(function () {
-//         $('#sopocachi-icon').css("transform","rotate(-10deg)");   
-//     }, function () {
-//         $('#sopocachi-icon').css("transform","rotate(0deg)");   
-//     });
-//     $('#map-santacruz-btn').hover(function () {
-//         $('#santacruz-icon').css("transform","rotate(10deg)");   
-//     }, function () {
-//         $('#santacruz-icon').css("transform","rotate(0deg)");   
-//     });
-
-//     // MENU *******************************************************************
-//     $('.btn-ver-menu').click(function () { 
-//         $('.pagina-izquierda').addClass("pagina-izquierda-2");
-//         setTimeout(function(){
-//             $('.pagina-derecha').addClass("pagina-derecha-2");
-//         },300)
-//     });
-//     $('.cerrar-menu').click(function () { 
-//         $('.pagina-derecha').removeClass("pagina-derecha-2");
-//         setTimeout(function(){
-//             $('.pagina-izquierda').removeClass("pagina-izquierda-2");
-//         },700)
-//     });
-// });
-
 window.addEventListener('load', inicio);
 
 function inicio(){
+    /* Animacion loader */
+    desaparecerLoader();
+    
+    /* Evento menu */
     document.getElementById('btn-menu').addEventListener('click', desplegarMenu);
 }
     
+function desaparecerLoader(){
+    document.getElementById("loader").style.transition="10s";
+    document.getElementById("loader").style.visibility="hidden";
+
+    document.getElementById("loader").style.display= "none";
+}
 function desplegarMenu() {
     document.getElementById('navpage').classList.toggle('mostrar-nav');
 }
