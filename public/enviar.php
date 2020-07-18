cd395df1e33a1c503ef4230772eb2002d5d5a921
CristhianChaucaMariaca <cristhiancchm+git@gmail.com>
2020-07-18 19:39:55 -0400

    ser modifico el tiempo de espera para redireccionar al index en el archivo gracias.html al momento de enviar el formulario

--- public/enviar.php
+++ public/enviar.php
@@ -6,7 +6,8 @@
     $correo = $_POST['correo'];
     $comentario = $_POST['comentario'];
     $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nComentario: " . $comentario;
+    $header = "Enviado desde Ichuri Coffee";
     
-    mail($destino, $asunto,$contenido);
+    mail($destino, $asunto,$contenido, $header);
     header('Location:gracias.html');
 ?>
